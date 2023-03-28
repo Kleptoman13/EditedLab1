7da4197f7545523a7bb988e0decefbc21d2d4452
Kleptoman13 <asdfghjkl38939@gmail.com>
2023-03-28 12:45:02 +0300

    Something

--- js/script.js
+++ js/script.js
@@ -1,45 +1,65 @@
 const $list = document.querySelector('.list');
 const $field = document.querySelector('.input-searcher');
-const $count = document.querySelector('.count-container')
+const $count = document.querySelector('.count-container');
+
 
 let list = null;   
 let testList = null; 
 
-if (!!getLS('list')){
-    list = getLS('list');
-    listGenerator(list);
+if (getLS('list').length == 0)
+{
+    fetch('https://pokeapi.co/api/v2/pokemon')
+        .then(function (responce)
+        {
+            return responce.json();
+        })
+        .then(function (data)
+        {
+            list = data.results;
+            for (let i = 0; i < list.length; i++) {
+                list[i].index = i;
+            }
+            listGenerator(list);
+            setLS('list', list);
+        })
+}
+else{
+    listGenerator(getLS('list'));
 }
 
 function listGenerator(list)
-{
+{   
+    console.log()
+    let count = 0;
     let template = '';
     if (list.length){
         for (let i = 0; i < list.length; i++) {
-            template += '<div class="user"><div class="info"><div class="container-name"><img class="pokemon-icon" src="svg/pokemon.svg" alt="#"><p class="name">'+ list[i].name +'</p></div><p class="url">'+ list[i].url +'/p></div><div class="container-btn"><input type="button" index="'+ i +'" value="Delete" class="btn"></div></div>'
+            fetch(list[i].url)
+            .then(function (responce)
+            {
+                return responce.json();
+            })
+            .then(function (data){
+                template += '<div class="user"><div class="info"><div class="info-container"><div class="img-pok-container"><img src="'+ data.sprites.front_default +'" alt="#" class="img-pok"></div>'+
+                    '<div class="container-name"><p class="name">'+ data.name +'</p></div>'+
+                    '<div><p class="hp">HP: '+ data.stats[0].base_stat +'</p></div>'+
+                    '<div><p class="attack">Attack: '+ data.stats[1].base_stat +'</p></div>'+
+                    '<div><p class="defense">Defense: '+ data.stats[2].base_stat +'</p></div>'+
+                    '<div><p class="speed">Speed: '+ data.stats[5].base_stat +'</p></div></div></div>'+
+                    '<div class="container-btn"><input type="button" index="'+ list[i].index +'" value="Delete" class="btn"></div></div>'
+
+                count++;
+                $count.innerHTML = '<p class="count">'+ count +' pokemons</p>';
+                $list.innerHTML = template;
+
+            });
         }
     }
     else{
         template += '<div><h1 class"error">Not found</h1></div>';
     }
-    $list.innerHTML = template;
-    $count.innerHTML = '<p class="count">'+ list.length +' pokemons</p>';
-    // setLS('list', list);
-
-    const $btnDelete = document.querySelectorAll('.btn');
-    for (let i = 0; i < $btnDelete.length; i++) {
-        $btnDelete[i].addEventListener('click', function() {
-            deleteUser(i, list);
-        })
-    }
 };
 
-// fetch('https://pokeapi.co/api/v2/pokemon')
-//  .then((resp) => resp.json())
-//  .then(function(data){
-//     list = data.results;
-//     listGenerator(list);
-    
-//  });
 
 function setLS(key, value){
     localStorage.setItem(key, JSON.stringify(value));
@@ -49,6 +69,7 @@ function getLS(key) {
 }
 
 $field.addEventListener('input', function() {
+    list = getLS('list');
     let query = this.value.toLowerCase();
     let filterdList = list.filter(function (el) {
         return ~el.name.toLowerCase().indexOf(query);
@@ -57,16 +78,18 @@ $field.addEventListener('input', function() {
     listGenerator(filterdList);
 })
 
-function deleteUser(index, list){
-    console.log(index);
-    console.log(list);
-    let templateList = list.filter(function(value, number){
-        return number !== index;
-    })
-    // console.log(templateList)
-    setLS('list', templateList);
-    listGenerator(templateList);
-}
 
-
+$list.addEventListener('click', function(e) {
+    if(e.target.classList.contains('btn')) {
+        let templateList = getLS('list');
+        console.log(templateList);
+        let id = e.target.getAttribute('index');
+        let id2 = templateList.findIndex(pokemon => pokemon.index == id);
+        templateList.splice(id2, 1);
+        console.log(templateList);
+        setLS('list', templateList);
+        $field.value = '';
+        listGenerator(getLS('list'));
+    }
+})
 
